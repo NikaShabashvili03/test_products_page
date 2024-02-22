2ceb3ea3c42e6fb9a32a6828547148d44f6c5b4a
Nika Shabashvili <nikashabashvili@Nikas-MacBook-Pro.local>
2024-02-22 22:23:55 +0400

    newcommit

--- src/App.js
+++ src/App.js
@@ -16,20 +16,22 @@ const generateAuthHeader = () => {
   };
 };
 
-
 const types = [
+  'none',
   'brand',
-  'price',
+  'price'
 ]
 
 const App = () => {
   const [filterType, setFilterType] = useState(types[0])
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
+
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [disabled, setDisabled] = useState(true);
+  const [totalPages, setTotalPages] = useState(0);
   const [fields, setFields] = useState([]);
   const [selectedField, setSelectedField] = useState('');
-  const [filteredProducts, setFilteredProducts] = useState([]);
-  const [disabled, setDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const fetchFields = async (field) => {
     setDisabled(true)
@@ -52,150 +54,169 @@ const App = () => {
     }
   };
 
-  const filterProducts = async (field, value) => {
-    try {
+  // Fetch Products By Ids
+  const fetchProductDetails = async (ids) => {
+    setDisabled(true)
+    await axios.post(
+      API_URL,
+      { action: 'get_items', params: { ids: ids } },
+      generateAuthHeader()
+    ).then((res) => {
+      setDisabled(false)
+      setProducts(res.data.result);
+    }).catch((err) => {
+      setDisabled(false)
+      console.log(err)
+    })
+  }
+
+  const fetchMaxTotalPage = async () => {
+    await axios.post(
+      API_URL,
+      {action: 'get_ids'},
+      generateAuthHeader()
+    ).then((res) => {
+      setTotalPages(Math.ceil(res.data.result.length / 50));
+    })
+  }
+
+  // Fetch Products Ids With offset
+  const fetchData = async (filterType) => {
+    setDisabled(true)
+    if(filterType === 'none'){      
+      await axios.post(
+        API_URL,
+        { action: 'get_ids', params: { offset: 0, offset: currentPage*50, limit: 50 } },
+        generateAuthHeader()
+      ).then((res) => {
+        setDisabled(false)
+        const uniqueIds = Array.from(new Set(res.data.result));
+        fetchProductDetails(uniqueIds);
+      }).catch((err) => {
+        setDisabled(false)
+        console.log(err)
+      })
+    }
+    else{
       await axios.post(
         API_URL,
         {
           action: 'filter',
-          params: { [field] : value },
+          params: { [filterType] : selectedField },
         },
         generateAuthHeader()
       ).then((response) => {
-        setFilteredProducts([]);
+        setDisabled(false)
         fetchProductDetails(response.data.result);
+      }).catch((err) => {
+        console.log(err)
       })
-    } catch (error) {
-      console.error('Error filtering products:', error);
-      return [];
     }
-  };
+  }
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.post(
-        API_URL,
-        { action: 'get_ids', params: { offset: 0, limit: 50 } },
-        generateAuthHeader(),
-      );
-
-      const uniqueIds = Array.from(new Set(response.data.result));
-      setFilteredProducts([])
-      fetchProductDetails(uniqueIds);
-      setTotalPages(Math.ceil(filteredProducts.length / 50));
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
-  const fetchProductDetails = async (ids) => {
-    ids?.map(async (id) => {
-      try {
-        await axios.post(
-          API_URL,
-          {
-            action: 'get_items',
-            params: { ids: [id] },
-          },
-          generateAuthHeader(),
-        ).then((response) => {
-          response.data.result[0] && setFilteredProducts((prev) => [...prev, response.data.result[0]]);
-        });
-
-      } catch (error) {
-        console.error(`Error fetching product details for ID ${id}:`, error);
-      }
-    });
-
-  };
-
-  useEffect(() => {
-    fetchFields(filterType).then(setFields);
-  }, [filterType]);
-
-  const handleFilter = async () => {
-    if (selectedField && filterType) {
-      filterProducts(filterType, selectedField);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [currentPage]);
-
+  // Change Page with pagination
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
   };
-  
-  return (
-    <div>
-      <h1>Product List</h1>
 
-      <div>
-        <label>Select Field:</label>
-        <select
-          value={selectedField}
-          disabled={disabled}
-          onChange={(e) => setSelectedField(e.target.value)}
-        >
-          <option value="">Select Field</option>
-          {fields.map((field) => {
-            if(field == null) return;
-            return (
-              <option key={field} value={field}>
-                {field}
-              </option>
+  // First And Change current page render
+  useEffect(() => {
+    fetchData(filterType);
+  }, [selectedField, currentPage])
+
+  useEffect(() => {
+    fetchFields(filterType).then(setFields);
+  }, [filterType])
+
+  
+  useEffect(() => {
+    fetchMaxTotalPage();
+  }, [])
+  return (
+    <div className='flex flex-col'>
+        {/* Filter */}
+
+        <div className='w-full mt-5 mx-5 flex-col flex justify-start items-start'>
+          <div>
+            <label className='mr-2'>Select Field:</label>
+            <select
+                  value={filterType}
+                  onChange={(e) => {
+                    setFilterType(e.target.value)
+                    setSelectedField('')
+                  }}
+                >
+                  {types?.map((field) => {
+                    return (
+                      <option key={field} value={field}>
+                        {field}
+                      </option>
+                    )
+                  })}
+              </select>
+          </div>
+          {
+            fields.length > 0 && (
+              <select
+                  className='mt-5'
+                  value={selectedField}
+                  disabled={disabled}
+                  onChange={(e) => setSelectedField(e.target.value)}
+                  >
+                        <option value="">Select Field</option>
+                        {fields.map((field) => {
+                          if(field == null) return;
+                          return (
+                            <option key={field} value={field}>
+                              {field}
+                            </option>
+                          )
+                        })}
+              </select>
             )
-          })}
-        </select>
-      </div>
-      <div>
-        <select
-            value={filterType}
-            onChange={(e) => setFilterType(e.target.value)}
-          >
-            {types?.map((field) => {
+          }
+        </div>
+
+        {/* Pagination */}
+        {!disabled && (
+          <div className='w-full my-[50px] flex justify-center items-center'>
+            <button className='border mx-2 disabled:opacity-50 w-[150px] py-1 rounded-2xl' onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 0 || disabled}>
+              Previous Page
+            </button>
+            {filterType === 'none' && <span className='mx-5'>{`${currentPage}/${totalPages}`}</span>}
+            <button className='border mx-2 disabled:opacity-50 w-[150px] py-1 rounded-2xl' onClick={() => handlePageChange(currentPage + 1)} disabled={disabled || totalPages === currentPage}>
+              Next Page
+            </button>
+        </div>
+        )}
+
+        {/* Products */}
+        {!disabled ? (
+          <div className='grid grid-cols-4 mt-[100px] gap-5'>
+            {products?.map((product, i) => {
               return (
-                <option key={field} value={field}>
-                  {field}
-                </option>
+                <ul className='w-full relative bg-gray-200 flex flex-col justify-between rounded-2xl shadow p-5 ' key={i}>
+                  <li className='text-center'>
+                    <h2 className='text-center font-bold'>Product</h2>
+                    {product?.product}
+                  </li>
+                  <li className='text-center'>
+                    <h2 className='text-center font-bold'>Price</h2>
+                    {product?.price}$
+                  </li>
+                  <li className='text-center'>
+                    <h2 className='text-center font-bold'>Brand</h2>
+                    {product?.brand || '-'}
+                  </li>
+                </ul>
               )
             })}
-          </select>
-      </div>
-      <button onClick={handleFilter}>Filter</button>
-      <button onClick={fetchData}>Reset</button>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Brand</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredProducts?.map((product, i) => (
-            <tr key={product?.id}>
-              <td>{i}</td>
-              <td>{product?.id}</td>
-              <td>{product?.product}</td>
-              <td>{product?.price}</td>
-              <td>{product?.brand || '-'}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <div>
-        <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
-          Previous Page
-        </button>
-        <span>{`Page ${currentPage} of ${totalPages}`}</span>
-        <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>
-          Next Page
-        </button>
-      </div>
+          </div>
+        ) : (
+          <div className='w-full flex text-4xl justify-center items-center'>
+            Loading...
+          </div>
+        )}
     </div>
   );
 };
